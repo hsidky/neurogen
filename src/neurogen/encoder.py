6cda3a0ac9771afe6325fb936d62b4cf4bedd58b
mmvih <mmvihani@gmail.com>
2021-02-17 14:45:49 +0000

    added extra documentation

--- src/neurogen/encoder.py
+++ src/neurogen/encoder.py
@@ -238,7 +238,8 @@ def fulloctree_decomposition(mesh,
     mesh_dir = os.path.join(directory, mesh_subdirectory)
     os.makedirs(mesh_dir, exist_ok=True)
 
-    # Write manifest file.
+    # Write manifest file/fragment file as specified:
+    # https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/meshes.md
     chunk_shape = (max_mesh_vertex - min_mesh_vertex)/(2**num_lods)
     grid_origin = min_mesh_vertex
     vertex_offsets = np.array([[0., 0., 0.] for _ in range(num_lods)])
@@ -252,7 +253,7 @@ def fulloctree_decomposition(mesh,
         manifest_file.write(vertex_offsets.astype('<f').tobytes(order='C'))
         manifest_file.write(num_fragments_per_lod.astype('<I').tobytes())
 
-        # Write fragment file.
+        # Write fragment file
         with open(os.path.join(mesh_dir, f'{segment_id}'), 'wb') as fragment_file:
             
             for i in reversed(lods):
@@ -311,32 +312,71 @@ def fulloctree_decomposition(mesh,
 
 def generate_mesh_dataframe(vertices, minvertices, lod=0):
 
-    reachlevel = False
+    """ This function generates a dataframe that contains information 
+    on the levels of details based on density.  We bin the vertices to fit 
+    in the number nodes in the respective LOD. The function continues until
+    all fragments of the highest level of detail contains less than minvertices.
+    
+    Parameters
+    ----------
+    vertices : numpy array 
+        A 3x1 array of the mesh's vertices
+    minvertices : int
+        The maximum number of vertices within a fragment in 
+        the highest level of detail
+    lod : int
+        The current number of levels to create the mesh
+
+    Returns
+    -------
+    lod : int
+        The number of levels created for the mesh
+    mesh_lods : pandas dataframe
+        The dataframe containing information on the level of 
+        details
+    """
+    # Intialize necessary information
+    reachlevel = False 
     num_vertices = len(vertices)
 
+    # Initalize necessary information to prevent recalculating
     maxvertex = vertices.max(axis=0)
     minvertex = vertices.min(axis=0)
 
+    # Initialize the dataframes
+    # mesh_df contains the orginial vertices of the mesh
     mesh_df = pd.DataFrame(data=vertices, columns=["x", "y", "z"])
+    # mesh_lods contains information of all the level of details
     mesh_lods = pd.DataFrame(data=[[0, 0, 0, num_vertices, 0, None]], columns = "x y z Count LOD Decompose".split())
+    # mesh_lod will only contains information on the current level of detail
+        # will continuously get updated
     mesh_lod = pd.DataFrame(data=[], columns=["x", "y", "z", "Decompose"])
-    pd.set_option("display.max_rows", None, "display.max_columns", None)
 
     while reachlevel == False:
+        # define the number of nodes, the number of times the mesh gets sliced 
+            # in the level of detail
         numsplits = int((2**lod)+1)
         xsplits = np.linspace(start=minvertex[0], stop=maxvertex[0], num=numsplits)
         ysplits = np.linspace(start=minvertex[1], stop=maxvertex[1], num=numsplits)
         zsplits = np.linspace(start=minvertex[2], stop=maxvertex[2], num=numsplits)
 
+        # meshnodes_df will be the binned data of mesh_df
         meshnodes_df = mesh_df.copy()
         for x in range(numsplits-1):
             for y in range(numsplits-1):
                 for z in range(numsplits-1):
                     
+                    # if the (x,y,z) coordinates are within that range then they get binned
+                        # into that fragment
                     condx = (mesh_df["x"] >= xsplits[x]) & (mesh_df["x"] < xsplits[x+1])
                     condy = (mesh_df["y"] >= ysplits[y]) & (mesh_df["y"] < ysplits[y+1])
                     condz = (mesh_df["z"] >= zsplits[z]) & (mesh_df["z"] < zsplits[z+1])
 
+                    # This section looks at the previous level of detail.
+                        # If the previous level specifies that there is no need
+                        # to decompose that fragment, because it is less than 
+                        # the minimum_vertices, then it replaces those vertices 
+                        # in the next level of detail with empty values.
                     if not mesh_lod[(mesh_lod["x"] == x//2) & 
                                     (mesh_lod["y"] == y//2) &
                                     (mesh_lod["z"] == z//2) &
@@ -349,19 +389,24 @@ def generate_mesh_dataframe(vertices, minvertices, lod=0):
                         meshnodes_df.loc[condy,"y"] = y
                         meshnodes_df.loc[condz,"z"] = z
 
+        # The extents of the mesh needs to be within the last fragment
         meshnodes_df.loc[mesh_df["x"]==xsplits[-1],"x"] = x
         meshnodes_df.loc[mesh_df["y"]==ysplits[-1],"y"] = y
         meshnodes_df.loc[mesh_df["z"]==zsplits[-1],"z"] = z
 
+        # Count the binned data
         mesh_lod = meshnodes_df.groupby(["x", "y", "z"]).size().reset_index(name="Count")
         mesh_lod["LOD"] = lod
         mesh_lod["Decompose"] = np.where(mesh_lod["Count"] > minvertices, True, False)
         
+        # Append this level of details information to the output dataframe
         if lod == 0:
             mesh_lods = mesh_lod
         else:
             mesh_lods = mesh_lods.append(mesh_lod)
         
+        # If all the vertices are less than minimum_vertices, then stop 
+            # the while loop
         lod = lod+1
         stop_condition = (mesh_lod["Decompose"] == False)
 
@@ -369,7 +414,7 @@ def generate_mesh_dataframe(vertices, minvertices, lod=0):
             reachlevel = True
     mesh_lods = mesh_lods.reset_index(drop=True)
 
-    return minvertices, lod, mesh_lods
+    return lod, mesh_lods
 
 
 def density_decomposition(mesh,
@@ -379,6 +424,28 @@ def density_decomposition(mesh,
                         quantization_bits=16,
                         compression_level=5,
                         mesh_subdirectory='meshdir'):
+
+    """ Generates a Neuroglancer precomputed multiresolution mesh based
+    on the number of vertices within each fragment
+    
+    Parameters
+    ----------
+    mesh : trimesh.base.Trimesh 
+        A Trimesh mesh object to decompose.
+    segment_id : str
+        The ID of the segment to which the mesh belongs. 
+    directory : str
+        Neuroglancer precomputed volume directory.
+    minimum : int
+        The minimum number of vertices that a fragment needs to have in order a
+        stop breaking into more octrees.  Default is 1024
+    quantization_bits : int
+        Number of bits for mesh vertex quantization. Can only be 10 or 16. 
+    compression_level : int
+        Level of compression for Draco format.
+    mesh_subdirectory : str
+        Name of the mesh subdirectory within the Neuroglancer volume directory.    
+    """
     
     # Mesh values
     mesh_vertices = mesh.vertices
@@ -386,8 +453,8 @@ def density_decomposition(mesh,
     min_mesh_vertex = mesh_vertices.min(axis=0)
     clean_mesh(mesh)
 
-    # Need to get information on meshes LOD prior to decimation
-    minimum_vertices, num_lods, dataframe = generate_mesh_dataframe(vertices=mesh.vertices, 
+    # Need to get information on meshes LOD prior to decimation.
+    num_lods, dataframe = generate_mesh_dataframe(vertices=mesh.vertices, 
                                                                     minvertices=minimum_vertices,
                                                                     lod=0)
 
@@ -405,7 +472,8 @@ def density_decomposition(mesh,
     mesh_dir = os.path.join(directory, mesh_subdirectory)
     os.makedirs(mesh_dir, exist_ok=True)
 
-    # Write manifest file.
+    # Write manifest file/fragment file as specified:
+    # https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/meshes.md
     chunk_shape = (max_mesh_vertex - min_mesh_vertex)/(2**num_lods)
     grid_origin = min_mesh_vertex
     vertex_offsets = np.array([[0., 0., 0.] for _ in range(num_lods)])
@@ -428,6 +496,7 @@ def density_decomposition(mesh,
                 decimated_mesh = mesh.simplify_quadratic_decimation(num_faces_left[i])
                 clean_mesh(decimated_mesh)
 
+                # The scale is the same as the number of nodes in each dimension.
                 nodes_per_dim = scales[i]
 
                 # The vertices need to range from 0 to number of nodes in mesh
@@ -455,6 +524,7 @@ def density_decomposition(mesh,
                             condy = (lodframe["y"] == y)
                             condz = (lodframe["z"] == z)
 
+                            # Information must be present in dataframe
                             if (condx & condy & condz).any():
 
                                 # Initialize Quantizer.
@@ -464,7 +534,7 @@ def density_decomposition(mesh,
                                     input_origin=np.array([0,0,0]), 
                                     quantization_bits=quantization_bits
                                 )
-                                dracolen = 0
+                                dracolen = 0 # Append zero if fragment contains no vertices
                                 if (len(mesh_z.vertices) > 0):
                                         mesh_z.vertices = quantizer(mesh_z.vertices)
                                         draco = encode_mesh(mesh_z,compression_level=compression_level)
